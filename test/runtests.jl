a107a2d9646675441e4e7c8d5f3be14d8bae86ad
Mike J Innes <mike.j.innes@gmail.com>
2018-03-02 17:46:48 +0000

    tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,38 @@
-using Traceur
+using Traceur: warnings
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+warns_for(ws, x) = any(w -> contains(w.message, x), ws)
+warns_for(ws, x, xs...) = warns_for(ws, x) && warns_for(ws, xs...)
+
+y = 1
+
+@testset "Traceur" begin
+
+naive_relu(x) = x < 0 ? 0 : x
+
+ws = warnings(() -> naive_relu(1))
+@test isempty(ws)
+
+ws = warnings(() -> naive_relu(1.0))
+@test warns_for(ws, "returns")
+
+function naive_sum(xs)
+  s = 0
+  for x in xs
+    s += x
+  end
+  return s
+end
+
+ws = warnings(() -> naive_sum(1))
+@test isempty(ws)
+
+ws = warnings(() -> naive_sum(1.0))
+@test warns_for(ws, "assigned", "dispatch", "returns")
+
+f(x) = x+y
+
+ws = warnings(() -> f(1))
+@test warns_for(ws, "global", "dispatch", "returns")
+
+end
