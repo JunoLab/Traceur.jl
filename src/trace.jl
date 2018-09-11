b799872ddc2ab82dd4cab7c403281f8a780f2620
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2018-09-11 16:46:44 +0200

    ignore getproperty/setproperty! and also updated the set of analysed methods properly

--- src/trace.jl
+++ src/trace.jl
@@ -11,14 +11,16 @@ Trace(w) = Trace(Set(), w)
 
 isprimitive(f) = f isa Core.Builtin || f isa Core.IntrinsicFunction
 
-const ignored_methods = [@which((1,2)[1])]
+const ignored_methods = Set([@which((1,2)[1])])
+const ignored_functions = Set([getproperty, setproperty!])
 
 function Cassette.posthook(ctx::TraceurCtx, out, f, args...)
   C, T = DynamicCall(f, args...), typeof.((f, args))
   tra = ctx.metadata
-  (T ∈ tra.seen || isprimitive(f) || method(C) ∈ ignored_methods ||
-    method(C).module ∈ (Core, Core.Compiler)) && return nothing
+  (f ∈ ignored_functions || T ∈ tra.seen || isprimitive(f) ||
+    method(C) ∈ ignored_methods || method(C).module ∈ (Core, Core.Compiler)) && return nothing
 
+  push!(tra.seen, T)
   analyse((a...) -> tra.warn(Warning(a...)), C)
   return nothing
 end
