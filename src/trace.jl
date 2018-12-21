c42295a17a5a112acd3b9fe205e6ecf909e30df4
Jarrett Revels <jarrettrevels@gmail.com>
2018-12-21 09:27:46 -0500

    update Traceur to Cassette v0.2

--- src/trace.jl
+++ src/trace.jl
@@ -25,7 +25,7 @@ function Cassette.posthook(ctx::TraceurCtx, out, f, args...)
   return nothing
 end
 
-trace(w, f) = Cassette.overdub(TraceurCtx(metadata=Trace(w)), f)
+trace(w, f) = Cassette.recurse(TraceurCtx(metadata=Trace(w)), f)
 
 warntrace(f) = trace(warning_printer(), f)
 
