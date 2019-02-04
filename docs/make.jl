5a864b335f517ad708bf478251c0a43aae49ec39
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2019-02-04 18:00:41 +0100

    more docs setup

--- docs/make.jl
+++ docs/make.jl
@@ -7,9 +7,6 @@ makedocs(
     modules = [Traceur]
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/JunoLab/Traceur.jl.git"
+)
